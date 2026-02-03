728ab8b817d3dda2a23d952a932757cbdf4f78ed
finnonly <finnonly@outlook.com>
2026-02-03 09:49:12 +0800

    新增 event

--- Finnonly.Avalonia/Event.cs
+++ Finnonly.Avalonia/Event.cs
@@ -271,6 +271,29 @@ public sealed class RawEvent : EventBindBase
     }
 }
 
+public sealed class Event : EventBindBase
+{
+    public Event(string name) : base(name) { }
+    public Event(string name, object arg0) : base(name, arg0) { }
+    public Event(string name, object arg0, object arg1) : base(name, arg0, arg1) { }
+    public Event(string name, object arg0, object arg1, object arg2) : base(name, arg0, arg1, arg2) { }
+    public Event(string name, object arg0, object arg1, object arg2, object arg3) : base(name, arg0, arg1, arg2, arg3) { }
+    public Event(string name, object arg0, object arg1, object arg2, object arg3, object arg4) : base(name, arg0, arg1, arg2, arg3, arg4) { }
+    public Event(string name, object arg0, object arg1, object arg2, object arg3, object arg4, object arg5) : base(name, arg0, arg1, arg2, arg3, arg4, arg5) { }
+    public Event(string name, object arg0, object arg1, object arg2, object arg3, object arg4, object arg5, object arg6) : base(name, arg0, arg1, arg2, arg3, arg4, arg5, arg6) { }
+    public Event(string methodName, object[] args) : base(methodName, args) { }
+
+    public override EventHandler ProvideValue(IServiceProvider serviceProvider)
+    {
+        static void EmptyHandler(object? sender, EventArgs e)
+        { }
+
+        return EventHandlerInfo(serviceProvider) ? Invoke : EmptyHandler;
+    }
+}
+
+
+
 internal sealed class BindingHelper : StyledElement
 {
     private static readonly StyledProperty<object> ValueProperty =
